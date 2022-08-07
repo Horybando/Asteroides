cafa14876b2a4dd1695cf1f2c800a099ef3cdc56
Horybando <horybando@gmail.com>
2022-08-07 14:29:12 -0300

    Ajuste de volumen
    - cambio de musica en cada nivel

--- Juego/Meteorito/SectorMeteoritos.gd
+++ Juego/Meteorito/SectorMeteoritos.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 ## Atributos export
 export var cantidad_meteoritos:int = 10
-export var intervalo_spawn:float = 1.0
+export var intervalo_spawn:float = 0.5
 
 ##Atributos
 var spawners:Array
